7148a3007f04dcffb1ed61c002804db5f5d1375d
Jacob Erickson <Jakerson8@gmail.com>
2022-09-13 16:10:27 -0600

    part 5

---
+++ src/Components/Symbols.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
+const Symbols = () => {
+  return (
+
+    <div className='symbols'>Symbols</div>
+  )
+}
+
+export default Symbols
